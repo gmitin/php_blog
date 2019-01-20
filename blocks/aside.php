5f8676a4d64b6e77cb85fece910c128814240ca4
gmitin <gmitinwork@yandex.ru>
2019-01-20 16:18:51 +0300

    Первый ком

---
+++ blocks/aside.php
@@ -0,0 +1,12 @@
+<aside class="col-md-4">
+  <div class="p-3 mb-3 bg-warning rounded">
+    <h4><b>Интересные факты</b></h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Minus natus, totam fugiat nobis ratione ea non quidem accusantium voluptatum!
+      Architecto dolorum inventore suscipit ex ducimus vero, id tenetur impedit et.</p>
+  </div>
+  <div class="p-3 mb-3">
+    <img class="img-thumbnail" src="https://itproger.com/img/courses/1534230100.jpg">
+
+  </div>
+</aside>
---
+++ error.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Ошибка 404</title>
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/main.css">
+  <link rel="icon" href="/img/favicon.ico">
+
+</head>
+<body>
+  <?php require 'blocks/header.php'; ?>
+  Ошибка 404, перейдите <a href="/">на главную</a>
+  <?php require 'blocks/footer.php'; ?>
+
+
+</body>
+</html>
